eda73816d9c96e9fa104c5e63148084233425689
Vlad Andrei <vlad@devtonic.net>
2024-01-04 15:00:04 +0200

    Add CRUD options

--- website/forms.py
+++ website/forms.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
+
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput(attrs={"class": "form-control", "placeholder":"Email Address"}))
-    first_name = forms.CharField(label="", max_length="100", widget=forms.TextInput(attrs={"class": "form-control", "placeholder":"First Name"}))
-    last_name = forms.CharField(label="", max_length="100", widget=forms.TextInput(attrs={"class": "form-control", "placeholder":"Last Name"}))
+    email = forms.EmailField(label="", widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Email Address"}))
+    first_name = forms.CharField(label="", max_length="100", widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "First Name"}))
+    last_name = forms.CharField(label="", max_length="100", widget=forms.TextInput(
+        attrs={"class": "form-control", "placeholder": "Last Name"}))
 
     class Meta:
         model = User
@@ -27,4 +32,19 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
-        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.CharField(required=True)
+    phone = forms.CharField(required=True)
+    address = forms.CharField(required=True)
+    city = forms.CharField(required=True)
+    state = forms.CharField(required=True)
+    zipcode = forms.CharField(required=True)
+
+    class Meta:
+        model = Record
+        exclude = ("user",)
